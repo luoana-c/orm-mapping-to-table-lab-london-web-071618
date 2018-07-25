82ae21195ef87913f55068f7ca9f6a39ac66bc93
Luoana Chirita <luoana.livadariu@gmail.com>
2018-07-25 08:59:58 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,5 +1,5 @@
 class Student
-  attr_accessor :name, :grade, :id
+  attr_accessor :name, :grade
   attr_reader :id
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]  
@@ -8,4 +8,12 @@ class Student
     @grade = grade 
     @id = id 
   end 
+  
+  def self.create_table
+    sql = <<-SQL 
+      CREATE TABLE IF NOT EXISTS students (
+        id INTEGER PRIMARY KEY, 
+        name TEXT, 
+        grade INTEGER
+        )
 end
